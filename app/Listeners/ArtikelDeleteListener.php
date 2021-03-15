46ca3256b1834e37d48c0ce7464920767b0ea312
rahmathidayat9 <yeageirstsatu@gmail.com>
2021-03-15 23:38:59 +0700

    Update Artikel Event

--- app/Listeners/ArtikelDeleteListener.php
+++ app/Listeners/ArtikelDeleteListener.php
@@ -8,6 +8,7 @@ use App\Events\ArtikelDeleteEvent;
 use App\Models\Artikel;
 use File;
 use Str;
+use DomDocument;
 
 class ArtikelDeleteListener
 {
@@ -34,24 +35,13 @@ class ArtikelDeleteListener
         File::delete(public_path('uploads/img/artikel/'.$artikel->thumbnail));
         
         $content = $artikel->deskripsi;
-        $dom = new \DomDocument();
+        $dom = new DomDocument();
         $dom->loadHtml($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);    
         $images = $dom->getElementsByTagName('img');
 
-        foreach($images as $k => $img){
-            $data = $img->getAttribute('src');
-            $data = base64_decode($data);
-            $image_name= "/uploads/img/artikel/".$k;
-            $path = public_path().$image_name;
-
-            dd(file_get_contents($path, $data));
-            
-            $img->removeAttribute('src');
-            $img->setAttribute('src', $image_name);
-            
+        foreach($images as $img){
+            $data = $img->getAttribute('src'); 
+            File::delete(public_path($data));
         }
-
-        $content = $dom->saveHTML();
-        return $content;
     }
 }
